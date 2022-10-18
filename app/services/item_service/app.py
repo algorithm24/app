2de99a0f7370d35ce720bb6386d32dafed425291
algorithm24 <72335445+algorithm24@users.noreply.github.com>
2022-10-18 20:50:24 +0700

    Update app.py

--- app/services/item_service/app.py
+++ app/services/item_service/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/item/<uuid>', methods=['GET'])
 def show_item(uuid):
     if uuid == "-1":
-        result = {'SUCCESS': 'show all'}
+        result = {'SUCCESS': 'show wrong!'}
     else:
         result = {'SUCCESS': f'{uuid}'}
     return jsonify(result)
