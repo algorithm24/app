226dab838720914c39ec8e7a3cea2584c2cb4928
algorithm24 <72335445+algorithm24@users.noreply.github.com>
2022-10-19 14:26:50 +0700

    Add files via upload

---
+++ app/test/test.py
@@ -0,0 +1,11 @@
+import unittest
+
+class Test(unittest.TestCase):
+    def test_pass_1(self):
+        pass
+    
+    def test_pass_2(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main(Test())
